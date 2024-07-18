1a95a3121b0dd16c14b820165047c2a11bee53e2
shhiivvaam <life.shivam2394@gmail.com>
2024-07-18 17:16:07 +0530

    Adding workers, and Pubsub imple

--- worker/dist/index.js
+++ worker/dist/index.js
@@ -11,16 +11,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const redis_1 = require("redis");
 const client = (0, redis_1.createClient)();
-function main() {
+function processSubmission(submission) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield client.connect();
-        while (1) {
-            const response = yield client.brPop("problems", 0);
-            // const response = await client.rPop("problems");
-            yield new Promise((resolve) => setTimeout(resolve, 1000));
-            // send it to the pub sub
-            console.log("Processed users submission");
+        const { problemId, code, language } = JSON.parse(submission);
+        console.log(`Processing Submission for problemId: ${problemId}...`);
+        console.log(`Code: ${code}`);
+        console.log(`Language: ${language}`);
+        // Here will be the actual code submission processing logic
+        // Simulate processing delay
+        yield new Promise(resolve => setTimeout(resolve, 1000));
+        console.log(`Finished processing submission for problemId ${problemId}`);
+    });
+}
+function startWorker() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            console.log('Worker connected to Redis');
+            // actual logic implementation
+            while (true) {
+                try {
+                    const submission = yield client.brPop("problems", 0);
+                    yield processSubmission(submission.element);
+                }
+                catch (error) {
+                    console.log('Error processing Submission', error);
+                    // we can have the logic here to push the submission back into the queue or
+                    // use the AOF(Append Only File) to add the logs of the request received
+                }
+            }
+        }
+        catch (error) {
+            console.error("Failed to connect to Redis", error);
         }
     });
 }
-main();
+startWorker();
+// async function main() {
+//     await client.connect();
+//     while (1) {
+//         const response = await client.brPop("problems", 0);
+//         // const response = await client.rPop("problems");
+//         await new Promise((resolve) => setTimeout(resolve, 1000));
+//         // send it to the pub sub
+//         console.log("Processed users submission");
+//     }
+// }
+// main();
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,17 +1,65 @@
 import { createClient } from "redis";
 const client = createClient();
 
-async function main() {
-    await client.connect();
 
-    while (1) {
-        const response = await client.brPop("problems", 0);
-        // const response = await client.rPop("problems");
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+async function processSubmission(submission: string) {
+    const { problemId, code, language } = JSON.parse(submission);
 
-        // send it to the pub sub
-        console.log("Processed users submission");
+    console.log(`Processing Submission for problemId: ${problemId}...`);
+    console.log(`Code: ${code}`);
+    console.log(`Language: ${language}`);
+
+    // Here will be the actual code submission processing logic
+
+    // Simulate processing delay
+    // Send it to the Pub Sub
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log(`Finished processing submission for problemId ${problemId}`);
+}
+
+
+async function startWorker() {
+    try {
+        await client.connect();
+        console.log('Worker connected to Redis');
+
+        // actual logic implementation
+        while (true) {
+            try {
+                const submission = await client.brPop("problems", 0);
+                // const response = await client.rPop("problems");
+                await processSubmission(submission.element);
+            } catch (error) {
+                console.log('Error processing Submission', error);
+                // we can have the logic here to push the submission back into the queue or
+                // use the AOF(Append Only File) to add the logs of the request received
+            }
+        }
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
     }
 }
 
-main();
+startWorker();
+
+
+
+
+
+
+
+
+// async function main() {
+//     await client.connect();
+
+//     while (1) {
+//         const response = await client.brPop("problems", 0);
+//         // const response = await client.rPop("problems");
+//         await new Promise((resolve) => setTimeout(resolve, 1000));
+
+//         // send it to the pub sub
+//         console.log("Processed users submission");
+//     }
+// }
+
+// main();
